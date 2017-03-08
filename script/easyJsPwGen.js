a97520ae7c256daeaccf420a6833f986c754ec92
didierCH <clapasson@mac.com>
2017-03-08 10:18:39 +0100

    add first variables to js script

--- script/easyJsPwGen.js
+++ script/easyJsPwGen.js
@@ -1,2 +1,12 @@
-// alert("hello script")
+var pwLength = document.getElementById("inputPasswordLength");
+var pwLength = document.getElementById("inputLowerCaseCharacters");
+var pwLength = document.getElementById("inputUpperCaseCharacters");
+var pwLength = document.getElementById("inputNumbers");
+var pwLength = document.getElementById("inputSymbols");
+var pwLength = document.getElementById("inputQuantity");
+var pwLength = document.getElementById("inputExcludeSimilarCharacters");
+var pwLength = document.getElementById("inputMakeSpeakablePassword");
+var pwLength = document.getElementById("");
+var pwLength = document.getElementById("inputLowerCaseCharacters");
 
+console.log(inputExcludeSimilarCharacters.checked)
