7160bb29d012b68389cf4236ca208f730a2b6b56
didierCH <clapasson@mac.com>
2017-03-10 14:50:41 +0100

    erster entwurf für zufallsgenerator

--- script/easyJsPwGen.js
+++ script/easyJsPwGen.js
@@ -1,12 +1,30 @@
-var pwLength = document.getElementById("inputPasswordLength");
-var pwLength = document.getElementById("inputLowerCaseCharacters");
-var pwLength = document.getElementById("inputUpperCaseCharacters");
-var pwLength = document.getElementById("inputNumbers");
-var pwLength = document.getElementById("inputSymbols");
-var pwLength = document.getElementById("inputQuantity");
-var pwLength = document.getElementById("inputExcludeSimilarCharacters");
-var pwLength = document.getElementById("inputMakeSpeakablePassword");
-var pwLength = document.getElementById("");
-var pwLength = document.getElementById("inputLowerCaseCharacters");
+// variables to get user input
+var getPwLength = document.getElementById("inputPasswordLength");
+var getLowercaseChar = document.getElementById("inputLowerCaseCharacters");
+var getUppercaseChar = document.getElementById("inputUpperCaseCharacters");
+var getNumbers = document.getElementById("inputNumbers");
+var getSymbols = document.getElementById("inputSymbols");
+var getQuantity = document.getElementById("inputQuantity");
+var getExcludeSimChar = document.getElementById("inputExcludeSimilarCharacters");
+var getMakeSpeakablePW = document.getElementById("inputMakeSpeakablePassword");
+var getChooseFirstChar = document.getElementsByName("inputChooseFirstCharacter");
+var getChooseLastChar = document.getElementsByName("inputChooseLastCharacter");
+var getGeneratePassword = document.getElementById("generatePassword");
 
-console.log(inputExcludeSimilarCharacters.checked)
+// console.log(getChooseLastChar.item(0).checked)
+// getGeneratePassword.addEventListener("click", function() { alert("You have CLICKED!!!") });
+
+// variables to generate password
+var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "1234567890";
+
+
+// function for random numbers
+function randomNumbers () {
+    var random = Math.round( 26 * Math.random() + 1 );
+    return random;
+}
+
+var callRandomNum = randomNumbers();
+console.log(callRandomNum);
