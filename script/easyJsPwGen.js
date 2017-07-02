c084accca3eb9f826a47b86660ba2045c128974e
didierCH <clapasson@mac.com>
2017-07-02 15:15:08 +0200

    aufgeräumt

--- script/easyJsPwGen.js
+++ script/easyJsPwGen.js
@@ -1,88 +1,82 @@
-// action by click on button
 var getGeneratePassword = document.getElementById("generatePassword");
-getGeneratePassword.addEventListener("click", output, false);
 
+// action by click on button
+getGeneratePassword.addEventListener("click", output, false);
     
+
 // function to check choosen options and make a concatenated string
 function checkOptions() {
     // variables to get user input
-    var getLowercaseChar = document.getElementById("inputLowerCaseCharacters");
-    var getUppercaseChar = document.getElementById("inputUpperCaseCharacters");
-    var getNumbers = document.getElementById("inputNumbers");
-    var getSymbols = document.getElementById("inputSymbols");
-    var getExcludeSimChar = document.getElementById("inputExcludeSimilarCharacters");
-    var getMakeSpeakablePW = document.getElementById("inputMakeSpeakablePassword");
-    var getChooseFirstChar = document.getElementsByName("inputChooseFirstCharacter");
-    var getChooseLastChar = document.getElementsByName("inputChooseLastCharacter");
+    var getLowercaseChar = document.getElementById("inputLowerCaseCharacters"),
+        getUppercaseChar = document.getElementById("inputUpperCaseCharacters"),
+        getNumbers = document.getElementById("inputNumbers"),
+        getSymbols = document.getElementById("inputSymbols"),
+        getExcludeSimChar = document.getElementById("inputExcludeSimilarCharacters"),
+        getMakeSpeakablePW = document.getElementById("inputMakeSpeakablePassword"),
+        getChooseFirstChar = document.getElementsByName("inputChooseFirstCharacter"),
+        getChooseLastChar = document.getElementsByName("inputChooseLastCharacter");
 
     // variables to generate password
-    var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
-    var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var numbers = "1234567890";
-    var symbols = document.getElementById("actualSymbols").value;
+    var lowercaseChar = "abcdefghijklmnopqrstuvwxyz",
+        uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        numbers = "1234567890",
+        symbols = document.getElementById("actualSymbols").value;
 
-    var concatString = "";
+    // concatenated string
+    var concatenatedString = "";
 
     if (getLowercaseChar.checked === true) {
-        concatString += lowercaseChar;
+        concatenatedString += lowercaseChar;
     } 
 
     if (getUppercaseChar.checked === true) {
-        concatString += uppercaseChar;
+        concatenatedString += uppercaseChar;
     } 
 
     if (getNumbers.checked === true) {
-        concatString += numbers;
+        concatenatedString += numbers;
     } 
 
     if (getSymbols.checked === true) {
-        concatString += symbols;
+        concatenatedString += symbols;
     }
 
-    console.log(concatString);
-    return concatString;
+    return concatenatedString;
 }
-// var checkOptionsResult = checkOptions();
+
 
 // function for random numbers
 function randomNumbers(receivedString) {
-    var stringLen = receivedString.length;
-    console.log(stringLen);
-    var random = Math.round((stringLen - 1) * Math.random() + 0 );
+    var stringLen = receivedString.length,
+    random = Math.round((stringLen - 1) * Math.random() + 0 );
 
-    console.log(random);
     return random;
 }
 
 // get random character from string
 function getCharFromString(receivedString) {
-    var randomNumber = randomNumbers(receivedString);
-    console.log(randomNumber);
-    
-    var actualChar = receivedString.charAt(randomNumber);
+    var randomNumber = randomNumbers(receivedString),
+        actualChar = receivedString.charAt(randomNumber);
 
-    console.log(actualChar);
     return actualChar;
 }
 
 
-
+// call functions and output result
 function output() {
-    var password = "";
-    var i;
-    var j;
-    var passwordChar;
+    var getQuantity = document.getElementById("inputQuantity").value,
+        getOutputQuantity = document.getElementById("outputQuantity").innerHTML = getQuantity,
+        getPwLength = document.getElementById("inputPasswordLength").value;
+
+    var password = "",
+        passwordChar;
+
+    var i,
+        j;
+
     var checkOptionsResult = checkOptions();
-    var getQuantity = document.getElementById("inputQuantity").value;
-    var getOutputQuantity = document.getElementById("outputQuantity").innerHTML = getQuantity;
-
-    // get password length
-    var getPwLength = document.getElementById("inputPasswordLength").value;
-    console.log(getPwLength);
-
 
     for (i = 0; i < getQuantity; i++) {
-        console.log(getQuantity);
         for (j = 0; j < getPwLength; j++) {
             passwordChar = getCharFromString(checkOptionsResult);
             password += passwordChar;
